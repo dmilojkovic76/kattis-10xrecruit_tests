07aba121d34ddba856187ba6b7b840aecc8fdeb8
Dusan Milojkovic <dmilojkovic76@gmail.com>
2019-08-03 14:36:21 +0200

    Started work on Calculate blood amounts function

--- royal_heir/index.js
+++ royal_heir/index.js
@@ -4,19 +4,37 @@ let inputData = [];
 let persons = [];
 
 class Person {
-	constructor (_name, _blood) {
+	constructor (_name, _blood, _parent1, _parent2) {
 		this.name = _name;
 		this.blood = _blood;
+		this.parent1 = _parent1;
+		this.parent2 = _parent2;
 	}
 
 	
 }
+
+/**
+ * Return the amount of blood of the founder
+ * @param {String} claimer 
+ * @param {Array} list 
+ */
 const calculateBlood = (claimer, list) => {
-	for (let i = 0; i < list.length; i++) {
-		const child = list[i].split(' ')[0];
-		const parent1 = list[i].split(' ')[0];
-	}
-	return 0;
+	console.log(`Calculating blood amount for claimer: ${claimer.name}`);
+	
+		for (let i = 0; i < list.length; i++) {			
+			if (persons[i + 1].blood === 0) {
+				const iClaimer = list[i].split(' ')[0];
+				if (iClaimer === claimer){
+					const iParent1 = list[i].split(' ')[1];
+					const iParent2 = list[i].split(' ')[2];
+					console.log(`processing data: ${iClaimer}'s parents: ${iParent1} and ${iParent2}`);
+					iClaimer.blood = calculateBlood(iClaimer, list);
+				}
+			}
+		}
+		return claimer.blood;
+	
 };
 
 
@@ -30,26 +48,26 @@ r1.on('line', (input) => {
 });
 
 r1.on('close', () => {
-	const relatives = parseInt(inputData[0].split(' ')[0]);
-	const claimers = parseInt(inputData[0].split(' ')[1]);
+	const relativesCount = parseInt(inputData[0].split(' ')[0]);
+	const claimersCount = parseInt(inputData[0].split(' ')[1]);
 	const founder = inputData[1];
-	const relativesArr = inputData.slice(2, 2 + relatives);
-	const claimersArr = inputData.slice(2 + relatives);
-	persons.push(new Person(founder, 1));
-	for (let i = 0; i < claimers; i++){
-		persons.push(new Person(claimersArr[i], 0));
+	const relativesArr = inputData.slice(2, 2 + relativesCount);
+	const claimersArr = inputData.slice(2 + relativesCount);
+	persons.push(new Person(founder, 1, '', ''));
+	for (let i = 0; i < claimersCount; i++){
+		persons.push(new Person(claimersArr[i], 0, '', ''));
 	}
 
 	for (let i = 0; i < persons.length; i++){
 		if (persons[i].name != founder)
-			persons[i].blood = calculateBlood(persons[i], relativesArr)
+			persons[i].blood = calculateBlood(persons[i], relativesArr);
 	}
 
 	console.log();	
 	console.log('Finished reading the file.');
 	console.log(inputData);
-	console.log(`Number of relatives: ${relatives}`);
-	console.log(`Number of claimers: ${claimers}`);
+	console.log(`Number of relatives: ${relativesCount}`);
+	console.log(`Number of claimers: ${claimersCount}`);
 	console.log(`The Founder: ${founder}`);
 	console.log('Relatives:');
 	console.log(relativesArr);
